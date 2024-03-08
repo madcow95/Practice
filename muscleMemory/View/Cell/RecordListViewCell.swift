7a6af0351161d3ea0ae23388fbda939d2c5a769e
lobaksetve@naver.com <lobasketve@naver.com>
2024-03-08 18:25:45 +0900

    Error. present가 문제일까? 1. Storyboard에서 ViewController -> ViewController로 화면을 전환할 때, 에러가 자꾸 발생 2. 그냥 전부 코드로 짜버릴까

--- muscleMemory/View/Cell/RecordListViewCell.swift
+++ muscleMemory/View/Cell/RecordListViewCell.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class RecordListViewCell: UITableViewCell {
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    /*
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
     }
@@ -15,4 +18,5 @@ class RecordListViewCell: UITableViewCell {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    */
 }
--- muscleMemory/View/RecordListViewController.swift
+++ muscleMemory/View/RecordListViewController.swift
@@ -9,38 +9,42 @@ import UIKit
 
 class RecordListViewController: UIViewController {
     
+    @IBOutlet weak var recordListTable: UITableView!
     
-    private let recordListTable: UITableView = {
-        let table = UITableView()
-        table.translatesAutoresizingMaskIntoConstraints = false
-        
-        return table
-    }()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    /*
+     override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
         view.backgroundColor = .white
         
         recordListTable.delegate = self
         recordListTable.dataSource = self
-        
-        recordListTable.backgroundColor = .red
-        view.addSubview(recordListTable)
-        recordListTable.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        recordListTable.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
+    */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+         view.backgroundColor = .white
+         
+         recordListTable.delegate = self
+         recordListTable.dataSource = self
+    }
+    
 }
 
-extension RecordListViewController: UITableViewDelegate, UITableViewDataSource {
+extension RecordListViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "", for: indexPath) as? RecordListViewCell else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "RecordListViewCell", for: indexPath) as? RecordListViewCell else {
             return UITableViewCell()
         }
+        // print(cell)
+         cell.firstLabel.text = "TEST"
         
         return cell
     }
