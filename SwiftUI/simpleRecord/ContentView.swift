80123dba017ee1eba5df984eb685900124e0c3a1
MadCow <lobasketve@naver.com>
2024-04-03 23:01:30 +0900

    Feat. SwiftUI버전 Simple Record 프로젝트 생성

---
+++ SwiftUI/simpleRecord/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  simpleRecord
+//
+//  Created by MadCow on 2024/4/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/simpleRecord/simpleRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleRecordApp.swift
+//  simpleRecord
+//
+//  Created by MadCow on 2024/4/3.
+//
+
+import SwiftUI
+
+@main
+struct simpleRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
