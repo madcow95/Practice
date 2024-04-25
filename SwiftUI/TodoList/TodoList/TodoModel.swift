8d01b4540b13c85ae0d4b19619eed9b5b548588d
MadCow <lobasketve@naver.com>
2024-04-25 15:02:55 +0900

    진짜 심플한 Todo List initial commit

---
+++ SwiftUI/TodoList/TodoList/TodoModel.swift
@@ -0,0 +1,33 @@
+//
+//  TodoModel.swift
+//  TodoList
+//
+//  Created by MadCow on 2024/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@Model
+class TodoModel {
+    let id: UUID = UUID()
+    var completed: Bool
+    var todoDescription: String
+    var priority: Priority
+    
+    init(completed: Bool, todoDescription: String, priority: Priority) {
+        self.completed = completed
+        self.todoDescription = todoDescription
+        self.priority = priority
+    }
+}
+
+enum Priority: String, Codable, Comparable {
+    static func < (lhs: Priority, rhs: Priority) -> Bool {
+        return lhs.rawValue < rhs.rawValue
+    }
+    
+    case high = "높음"
+    case medium = "중간"
+    case low = "낮음"
+}
