018b7f2fa1e0d22fde38a9fdd0a5e8a1bceacca7
MadCow <lobasketve@naver.com>
2024-11-02 22:28:21 +0900

    11/2 코딩테스트 - 1

---
+++ CodingTest/241031.playground/Contents.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ number:String) -> Int {
+    let n = Decimal(string: number)!
+//    let sum = number.map { Int(String($0))! }.reduce(0, +)
+//    print(sum)
+    
+    return 0
+}
+
+print(solution("78720646226947352489"))
---
+++ CodingTest/241102.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/*
+ MARK: 수열과 구간 쿼리 1
+func solution(_ arr:[Int], _ queries:[[Int]]) -> [Int] {
+    var copiedArr = arr
+    for query in queries {
+        for i in query[0]...query[1] {
+            copiedArr[i] += 1
+        }
+    }
+    return copiedArr
+}
+
+print(solution([0, 1, 2, 3, 4], [[0, 1],[1, 2],[2, 3]])) // [1,3,4,4,4]
+*/
+
