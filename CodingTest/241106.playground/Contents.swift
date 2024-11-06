b9a3e3885e7fa334c11fb50caae3735abfe5eae3
MadCow <lobasketve@naver.com>
2024-11-06 23:31:08 +0900

    241106 코딩테스트 - 2

--- CodingTest/241106.playground/Contents.swift
+++ CodingTest/241106.playground/Contents.swift
@@ -109,3 +109,18 @@ func solution(_ str_list:[String]) -> [String] {
 
 print(solution(["u", "u", "r", "l"]))
 */
+
+
+/*
+ MARK: 공 던지기
+ https://school.programmers.co.kr/learn/courses/30/lessons/120843?language=swift
+func solution(_ numbers:[Int], _ k:Int) -> Int {
+    let index = ((k-1) * 2) % numbers.count
+    
+    return numbers[index]
+}
+
+print(solution([1, 2, 3], 3))
+*/
+
+
