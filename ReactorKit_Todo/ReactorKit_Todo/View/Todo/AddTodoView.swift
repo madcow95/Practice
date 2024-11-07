f3b1cc32bfd90581864ebb4e4a6048ffb0195771
MadCow <lobasketve@naver.com>
2024-11-07 19:25:17 +0900

    Add. Todo 입력화면 추가

--- ReactorKit_Todo/ReactorKit_Todo/View/Todo/AddTodoView.swift
+++ ReactorKit_Todo/ReactorKit_Todo/View/Todo/AddTodoView.swift
@@ -10,22 +10,83 @@ import UIKit
 
 class AddTodoView: UIViewController {
     
-    private let button: UIButton = {
-        let button = UIButton()
-        button.setTitle("Add Todo", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         
-        return button
+        return scrollView
+    }()
+    private let contentView: UIView = {
+        let content = UIView()
+        content.translatesAutoresizingMaskIntoConstraints = false
+        
+        return content
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "제목"
+        label.textColor = .white
+        label.setContentHuggingPriority(.defaultHigh + 1, for: .horizontal)
+        label.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        
+        return label
+    }()
+    private let titleTextField: UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.placeholder = "제목을 입력해주세요."
+        tf.layer.cornerRadius = 10
+        tf.layer.borderWidth = 1
+        tf.layer.borderColor = UIColor.lightGray.cgColor
+        tf.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        tf.leftViewMode = .always
+        
+        return tf
+    }()
+    private lazy var titleStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [titleLabel, titleTextField])
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        return stackView
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBrown
-        view.addSubview(button)
         
-        button.snp.makeConstraints {
-            $0.left.right.equalToSuperview()
-            $0.bottom.equalToSuperview().offset(-100)
+        configureUI()
+    }
+    
+    func configureUI() {
+        configureScrollView()
+        configureTitle()
+    }
+    
+    func configureScrollView() {
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        scrollView.snp.makeConstraints {
+            $0.top.left.right.bottom.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints {
+            $0.top.left.right.bottom.equalToSuperview()
+            $0.width.equalTo(scrollView.snp.width)
+        }
+    }
+    
+    func configureTitle() {
+        contentView.addSubview(titleStackView)
+        
+        titleStackView.snp.makeConstraints {
+            $0.top.equalTo(contentView.snp.top).offset(10)
+            $0.left.equalTo(contentView.snp.left).offset(10)
+            $0.right.equalTo(contentView.snp.right).offset(-10)
         }
     }
 }
