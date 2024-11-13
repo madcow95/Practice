6905ff3ca658201a9aa047129e9276a9b9064bda
MadCow <lobasketve@naver.com>
2024-11-13 23:54:14 +0900

    Feat. Tabbar 추가

--- StackPractice/Flutter_Practice/lib/main.dart
+++ StackPractice/Flutter_Practice/lib/main.dart
@@ -1,7 +1,41 @@
+import 'package:contact/widget/bottom_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'MadCow',
+      theme: ThemeData(
+          colorScheme: ColorScheme.dark(
+            brightness: Brightness.dark,
+            secondary: Colors.white
+          ),
+      ),
+      // NeverScrollableScrollPhysics: 손가락 움직임으로 탭을 변경하지 않는다
+      home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                Container(),
+                Container(),
+                Container(),
+                Container()
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
+          )
+      ),
+    );
+  }
+}
