bd8b1d7b31dd544c4e26e7ff22d6bebda158b0c6
MadCow <lobasketve@naver.com>
2024-11-25 22:54:23 +0900

    Feat. 상세 화면 dart파일 생성

---
+++ StackPractice/Flutter_Practice/lib/screen/detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatefulWidget {
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
